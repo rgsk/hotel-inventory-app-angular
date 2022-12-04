3968108a60e83aba3fb1cc25de14c54492055164
rgsk <guptarahul70322@gmail.com>
2022-12-04 20:27:10 +0530

    bindings

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.css'],
 })
 export class RoomsComponent {
-
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = 312;
+  count = 0;
+  hideRooms = true;
+  increment() {
+    console.log('before', this.count);
+    this.count += 1;
+    console.log('after', this.count);
+  }
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 }
